825ac8db5c47d2197e16e802f96a74177cd10951
tibor-szalai <tibor@doit-intl.com>
2022-12-14 13:06:35 +0100

    Fix output values

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "iam_role_name" {
   description = "The name of the IAM Role"
-  value       = aws_iam_role.doit-management-role.name
+  value       = aws_iam_role.doit_management_role.name
 }
 
 output "iam_role_arn" {
   description = "The arn of the IAM Role"
-  value       = aws_iam_role.doit-management-role.arn
+  value       = aws_iam_role.doit_management_role.arn
 }
